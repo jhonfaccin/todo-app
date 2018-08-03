6b48386c9d108e9d00315526bbdf149435f9f9d5
jhon faccin <jhonfaccin@gmail.com>
2018-08-03 18:40:27 -0300

    evento onChange para o campo input

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -7,16 +7,24 @@ import TodoList from './todoList'
 export default class Todo extends Component{
     constructor (props){
         super (props)
+        this.state = {description: '',list: []}
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
     handleAdd(){
-        console.log(this)
+        console.log(this.state.description)
     }
-    render(){
+
+    handleChange(evento){
+        this.setState({...this.state,description: evento.target.value})
+    }
+
+    render(){ 
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}></TodoForm>
+                <TodoForm handleAdd={this.handleAdd} description={this.description}
+                handleChange={this.handleChange}></TodoForm>
                 <TodoList/>
             </div>
         )
